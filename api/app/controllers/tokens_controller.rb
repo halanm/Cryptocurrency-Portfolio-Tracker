d7d734675d2955af669b660fb152ffece117c62b
halanm <halanm123@gmail.com>
2025-06-02 19:22:34 -0700

    Refactor TokensController#index to fetch market data from CoinGecko API and include price, market cap, and change percentage in the response

--- api/app/controllers/tokens_controller.rb
+++ api/app/controllers/tokens_controller.rb
@@ -1,5 +1,21 @@
 class TokensController < ApplicationController
   def index
-    render json: Token.all
+    tokens = Token.all.as_json(except: [:created_at, :updated_at])
+    symbols = tokens.map { |token| token['symbol'].downcase }.uniq.join(',')
+    coingecko_response = Faraday.get("https://api.coingecko.com/api/v3/coins/markets", {
+      symbols: symbols,
+      vs_currency: 'usd'
+    }) do |req|
+      req.headers['x-cg-demo-api-key'] = ENV['COINGECKO_API_KEY']
+    end
+    token_data = JSON.parse(coingecko_response.body)
+    tokens.each do |token|
+      symbol = token['symbol'].downcase
+      market = token_data.find { |item| item['symbol'].downcase == symbol }
+      token['price'] = market ? market['current_price'] : nil
+      token['market_cap'] = market ? market['market_cap'] : nil
+      token['change_percentage'] = market ? market['price_change_percentage_24h'] : nil
+    end
+    render json: tokens
   end
 end
