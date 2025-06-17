659364f2827ddd1801b3c4d967f29416d1f6b114
halanm <halanm123@gmail.com>
2025-06-16 20:58:26 -0700

    Add .env.example for wallet connect configuration and integrate RainbowKit with Wagmi and React Query in main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,6 +7,13 @@ import "./index.css";
 
 import { createTheme, ThemeProvider } from "@mui/material";
 
+import "@rainbow-me/rainbowkit/styles.css";
+
+import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { WagmiProvider } from "wagmi";
+import { mainnet, base } from "wagmi/chains";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
 const theme = createTheme({
   palette: {
     primary: {
@@ -26,12 +33,26 @@ const theme = createTheme({
   cssVariables: true,
 });
 
+const config = getDefaultConfig({
+  appName: import.meta.env.VITE_WALLET_CONNECT_CLOUD_PROJECT_NAME || "CPT",
+  projectId: import.meta.env.VITE_WALLET_CONNECT_CLOUD_PROJECT_ID || "",
+  chains: [mainnet, base],
+});
+
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <AppRoutes />
-      </ThemeProvider>
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <RainbowKitProvider>
+            <ThemeProvider theme={theme}>
+              <AppRoutes />
+            </ThemeProvider>
+          </RainbowKitProvider>
+        </QueryClientProvider>
+      </WagmiProvider>
     </BrowserRouter>
   </StrictMode>
 );
