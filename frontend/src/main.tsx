0e9312118589b3e19201c7c2af928cbfca1b770d
halanm <halanm123@gmail.com>
2025-06-16 18:55:48 -0700

    Setup mui

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import { BrowserRouter } from 'react-router';
 import { AppRoutes } from './routes/AppRoutes';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import './index.css'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
