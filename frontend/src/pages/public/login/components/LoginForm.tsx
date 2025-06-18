485805d496c22de7805dceae2de563730e4ee830
halanm <halanm123@gmail.com>
2025-06-17 17:48:13 -0700

    Add LoginForm component for user authentication

---
+++ frontend/src/pages/public/login/components/LoginForm.tsx
@@ -0,0 +1,51 @@
+import { Box, Button, Link, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+
+export function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <Box component="form" noValidate>
+      <TextField
+        label="Email"
+        type="email"
+        fullWidth
+        margin="normal"
+        size="small"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+      />
+      <TextField
+        label="Password"
+        type="password"
+        fullWidth
+        margin="normal"
+        size="small"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        required
+      />
+      <Typography variant="body2" align="right" sx={{ mt: 2 }}>
+        <Link
+          href="/forgot-password"
+          variant="body2"
+          color="primary"
+          underline="none"
+        >
+          I forgot my password
+        </Link>
+      </Typography>
+      <Button
+        type="submit"
+        variant="contained"
+        color="primary"
+        fullWidth
+        sx={{ mt: 2 }}
+      >
+        Login
+      </Button>
+    </Box>
+  );
+}
