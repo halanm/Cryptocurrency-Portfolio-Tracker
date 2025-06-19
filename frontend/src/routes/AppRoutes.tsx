26d4b4403e819ff1c5401861f212af3478beabb7
halanm <halanm123@gmail.com>
2025-06-18 21:06:12 -0700

    Refactor AppRoutes to integrate authentication handling with AuthRoute and ProtectedRoute components

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { type RouteObject, useRoutes } from "react-router";
 
-import { publicRoutes } from "./public";
+import { authRoutes } from "./auth";
+import { privateRoutes } from "./private";
 import { fallbackRoutes } from "./fallback";
 
 import type { ReactElement } from "react";
+import { ProtectedRoute } from "../components/ProtectedRoute";
+import { AuthRoute } from "../components/AuthRoute";
 
 type Route = {
   path: string;
+  isPrivate?: boolean;
+  isAuth?: boolean;
   element: ReactElement;
 };
 
@@ -15,14 +20,25 @@ export function AppRoutes() {
   const parseRouteObjects = (routes: Route[]): RouteObject[] => {
     return routes.map((route) => ({
       path: route.path,
-      element: route.element,
+      element: route.isPrivate ? (
+        <ProtectedRoute>{route.element}</ProtectedRoute>
+      ) : route.isAuth ? (
+        <AuthRoute>{route.element}</AuthRoute>
+      ) : (
+        route.element
+      ),
     }));
   };
 
-  const publicRouteObjects = parseRouteObjects(publicRoutes);
+  const authRouteObjects = parseRouteObjects(authRoutes);
+  const privateRouteObjects = parseRouteObjects(privateRoutes);
   const fallbackRouteObjects = parseRouteObjects(fallbackRoutes);
 
-  const routes = [...publicRouteObjects, ...fallbackRouteObjects];
+  const routes = [
+    ...authRouteObjects,
+    ...privateRouteObjects,
+    ...fallbackRouteObjects,
+  ];
 
   const allRoutes = useRoutes(routes);
 
