9d2a4c41c4a9e523df333ecbea3049e356a025ae
halanm <halanm123@gmail.com>
2025-06-20 20:39:46 -0700

    Add TrendingTokensCard component to display trending cryptocurrency data

---
+++ frontend/src/pages/private/home/components/TrendingTokensCard.tsx
@@ -0,0 +1,105 @@
+import {
+  Box,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import { useGetTrendingCoins } from "../../../../hooks/coins/useGetTrendingCoins";
+
+import TrendingUp from "@mui/icons-material/TrendingUp";
+import TrendingDown from "@mui/icons-material/TrendingDown";
+
+export function TrendingTokensCard() {
+  const { data: trendingCoins } = useGetTrendingCoins();
+
+  return (
+    <Box
+      sx={{
+        backgroundColor: "var(--mui-palette-background-paper)",
+        border: "1px solid var(--mui-palette-secondary-dark)",
+        borderRadius: "8px",
+      }}
+    >
+      <Typography
+        variant="h6"
+        sx={{
+          padding: "16px",
+        }}
+      >
+        Trending Tokens
+      </Typography>
+      <TableContainer>
+        <Table aria-label="simple table">
+          <TableHead
+            sx={{ backgroundColor: "var(--mui-palette-secondary-main)" }}
+          >
+            <TableRow>
+              <TableCell>Pairs</TableCell>
+              <TableCell>Price</TableCell>
+              <TableCell>Change</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {trendingCoins?.map((coin) => (
+              <TableRow
+                key={coin.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      alignItems: "center",
+                    }}
+                  >
+                    <img
+                      src={coin.image}
+                      alt={coin.name}
+                      style={{
+                        width: "24px",
+                        height: "24px",
+                        marginRight: "8px",
+                      }}
+                    />
+                    <Typography variant="body1" textTransform={"uppercase"}>
+                      {coin.symbol} 🔥
+                    </Typography>
+                  </Box>
+                </TableCell>
+                <TableCell>${coin.current_price}</TableCell>
+                <TableCell>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      alignItems: "center",
+                      color:
+                        coin.price_change_percentage_24h_in_currency > 0
+                          ? "green"
+                          : "red",
+                    }}
+                  >
+                    <Typography variant="body1" sx={{ marginLeft: "8px" }}>
+                      {coin.price_change_percentage_24h_in_currency > 0
+                        ? "+"
+                        : ""}
+                      {coin.price_change_percentage_24h_in_currency.toFixed(3)}%
+                    </Typography>
+                    {coin.price_change_percentage_24h_in_currency > 0 ? (
+                      <TrendingUp sx={{ marginLeft: "8px" }} />
+                    ) : (
+                      <TrendingDown sx={{ marginLeft: "8px" }} />
+                    )}
+                  </Box>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
+  );
+}
