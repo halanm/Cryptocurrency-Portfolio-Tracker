426bfcc8663431d65a41e7ee7f1c7b552aed0e5c
halanm <halanm123@gmail.com>
2025-06-20 20:50:16 -0700

    Implement Dashboard layout with TrendingTokensCard, FixedTokenCards, and UserPortfoliosSummary components

--- frontend/src/pages/private/home/Dashboard.tsx
+++ frontend/src/pages/private/home/Dashboard.tsx
@@ -1,5 +1,25 @@
+import { Grid } from "@mui/material";
 import { PageContainer } from "../../../ui/PageContainer/PageContainer";
+import { TrendingTokensCard } from "./components/TrendingTokensCard";
+import { FixedTokenCards } from "./components/FixedTokenCards";
+import { UserPortfoliosSummary } from "./components/UserPortfoliosSummary";
 
 export default function Dashboard() {
-  return <PageContainer pageTitle="Dashboard"></PageContainer>;
+  return (
+    <PageContainer pageTitle="Dashboard">
+      <Grid container spacing={1}>
+        <Grid size={{ md: 5, xl: 4 }}>
+          <TrendingTokensCard />
+        </Grid>
+        <Grid size={{ md: 7, xl: 8 }} container spacing={2} direction="column">
+          <Grid>
+            <FixedTokenCards />
+          </Grid>
+          <Grid>
+            <UserPortfoliosSummary />
+          </Grid>
+        </Grid>
+      </Grid>
+    </PageContainer>
+  );
 }
