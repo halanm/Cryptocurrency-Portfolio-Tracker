76a1c9eaf4c7f8372f6eaa9c10b1e13d77998f65
halanm <halanm123@gmail.com>
2025-06-23 20:01:32 -0700

    Enhance Portfolio type by adding optional total_trades and tokens properties

--- frontend/src/domain/Portfolio.ts
+++ frontend/src/domain/Portfolio.ts
@@ -5,4 +5,15 @@ export type Portfolio = {
   total_return: number;
   current_value: number;
   change_24h_percentage: number;
+  total_trades?: number;
+  tokens?: {
+    symbol: string;
+    name: string;
+    image: string;
+    quantity: number;
+    current_price: number;
+    current_value: number;
+    total_invested: number;
+    price_change_24h: number;
+  }[];
 };
