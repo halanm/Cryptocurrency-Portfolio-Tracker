1813d784b02067b2750774106bcb52e1c464492a
halanm <halanm123@gmail.com>
2025-06-24 20:52:47 -0700

    Add useGetTrendingTokens hook for fetching trending token data

--- frontend/src/hooks/token/useGetTrendingTokens.ts
+++ frontend/src/hooks/token/useGetTrendingTokens.ts
@@ -1,15 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
-import type { Coin } from "../../domain/Coin";
+import type { Token } from "../../domain/Token";
 import { useUser } from "../user/useUserContext";
 import axios from "axios";
 
-export const useGetTrendingCoins = () => {
+export const useGetTrendingTokens = () => {
   const { user } = useUser();
 
   const preferred_currency = user?.preferred_currency || "usd";
 
-  return useQuery<Coin[] | null>({
-    queryKey: ["trending-coins"],
+  return useQuery<Token[] | null>({
+    queryKey: ["trending-tokens"],
     queryFn: async () => {
       const res = await axios.get(
         `https://api.coingecko.com/api/v3/coins/markets?order=volume_desc&vs_currency=${preferred_currency}&per_page=10&price_change_percentage=24h`
