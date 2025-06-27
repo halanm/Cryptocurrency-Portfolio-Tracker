8bd81d839aa7782d435d2b79f505bf491ff188b0
halanm <halanm123@gmail.com>
2025-06-26 19:01:28 -0700

    Add PortfolioSummary component to display detailed portfolio information

---
+++ frontend/src/pages/private/PortfolioDetails/components/PortfolioSummary.tsx
@@ -0,0 +1,79 @@
+import { Box, Grid, Stack, Typography } from "@mui/material";
+import type { Portfolio } from "../../../../domain/Portfolio";
+
+type PortfolioSummaryProps = {
+  portfolio?: Portfolio | null;
+};
+
+export function PortfolioSummary({ portfolio }: PortfolioSummaryProps) {
+  return portfolio ? (
+    <Grid size={8}>
+      <Box
+        sx={{
+          backgroundColor: "var(--mui-palette-background-paper)",
+          border: "1px solid var(--mui-palette-secondary-dark)",
+          borderRadius: "8px",
+          padding: "16px",
+        }}
+      >
+        <Typography variant="h6">Portfolio Summary</Typography>
+        <Box sx={{ marginTop: "16px" }}>
+          <Typography variant="body2">Total portfolio value</Typography>
+          <Typography variant="h5" sx={{ fontWeight: "bold" }}>
+            ${portfolio.current_value.toLocaleString()}
+          </Typography>
+          <Typography
+            variant="body2"
+            color={portfolio.change_24h_percentage < 0 ? "error" : "success"}
+          >
+            {portfolio.change_24h_percentage > 0 ? "+" : "-"}$
+            {Math.abs(portfolio.change_24h_percentage).toFixed(2)}
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            marginTop: "16px",
+            padding: "16px",
+            border: "1px solid var(--mui-palette-secondary-dark)",
+            borderRadius: "8px",
+          }}
+        >
+          <Stack direction="row" justifyContent="space-between">
+            <Typography variant="body2">Total invested</Typography>
+            <Typography variant="body2" sx={{ fontWeight: "bold" }}>
+              ${portfolio.total_invested.toLocaleString()}
+            </Typography>
+          </Stack>
+          <Stack
+            direction="row"
+            justifyContent="space-between"
+            sx={{ marginTop: "8px" }}
+          >
+            <Typography variant="body2">Total return</Typography>
+            <Typography
+              variant="body2"
+              sx={{
+                fontWeight: "bold",
+                color:
+                  portfolio.total_return >= 0 ? "success.main" : "error.main",
+              }}
+            >
+              {portfolio.total_return >= 0 ? "+" : "-"}$
+              {Math.abs(portfolio.total_return).toFixed(2)}
+            </Typography>
+          </Stack>
+          <Stack
+            direction="row"
+            justifyContent="space-between"
+            sx={{ marginTop: "8px" }}
+          >
+            <Typography variant="body2">Total trades</Typography>
+            <Typography variant="body2" sx={{ fontWeight: "bold" }}>
+              {portfolio.total_trades}
+            </Typography>
+          </Stack>
+        </Box>
+      </Box>
+    </Grid>
+  ) : null;
+}
