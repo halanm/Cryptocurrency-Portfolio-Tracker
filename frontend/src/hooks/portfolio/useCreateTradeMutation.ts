bc6a37e0fd8f238562d2c7f1f900d0bc146cb4d1
halanm <halanm123@gmail.com>
2025-06-27 20:56:35 -0700

    Add useCreateTradeMutation hook to handle trade creation in portfolios

---
+++ frontend/src/hooks/portfolio/useCreateTradeMutation.ts
@@ -0,0 +1,29 @@
+import { useMutation } from "@tanstack/react-query";
+import { api } from "../../lib/axiosInstance";
+import { AuthService } from "../../services/authService";
+
+interface TradeData {
+  token_symbol: string | null;
+  amount_invested: number | null;
+  trade_type: string | null;
+}
+
+type useCreateTradeMutation = {
+  portfolio_id?: string | number | null;
+  data: TradeData;
+};
+
+export const useCreateTradeMutation = () => {
+  const token = AuthService.getAccessToken();
+
+  return useMutation({
+    mutationFn: async ({ portfolio_id, data }: useCreateTradeMutation) => {
+      const res = await api.post(`/portfolios/${portfolio_id}/trades/`, data, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      return res.data;
+    },
+  });
+};
