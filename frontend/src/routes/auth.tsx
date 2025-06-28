eeda97de0aa2569ebdd5454b1d7e6dcb860dd524
halanm <halanm123@gmail.com>
2025-06-27 22:48:50 -0700

    Add signup route to authRoutes for user registration

--- frontend/src/routes/auth.tsx
+++ frontend/src/routes/auth.tsx
@@ -1,9 +1,15 @@
 import { Login } from "../pages/auth/login/Login";
+import { Register } from "../pages/auth/register/Register";
 
 export const authRoutes = [
   {
     path: "/login",
     element: <Login />,
+    isAuth: true,
+  },
+  {
+    path: "/signup",
+    element: <Register />,
     isAuth: true,
   },
 ];
