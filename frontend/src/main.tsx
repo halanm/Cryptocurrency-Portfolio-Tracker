64ba67a4856db54308cd284587a58d45797587f0
halanm <halanm123@gmail.com>
2025-06-30 18:45:07 -0700

    Refactor main.tsx to simplify rendering by using Providers and AlertCard components

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,63 +5,17 @@ import { AppRoutes } from "./routes/AppRoutes";
 
 import "./index.css";
 
-import { createTheme, ThemeProvider } from "@mui/material";
-
 import "@rainbow-me/rainbowkit/styles.css";
-
-import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
-import { WagmiProvider } from "wagmi";
-import { mainnet, base } from "wagmi/chains";
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-import { UserProvider } from "./providers/UserProvider";
-
-const theme = createTheme({
-  palette: {
-    background: {
-      default: "#f7f7f7",
-      paper: "#ffffff",
-    },
-    primary: {
-      main: "#0060f2",
-    },
-    secondary: {
-      light: "#f0f0f0",
-      main: "#e0e0e0",
-      dark: "#c0c0c0",
-    },
-    text: {
-      primary: "#0d0d0d",
-      secondary: "#696969",
-    },
-  },
-  typography: {
-    fontFamily: ["Inter", "Space Grotesk"].join(","),
-  },
-  cssVariables: true,
-});
-
-const config = getDefaultConfig({
-  appName: import.meta.env.VITE_WALLET_CONNECT_CLOUD_PROJECT_NAME || "CPT",
-  projectId: import.meta.env.VITE_WALLET_CONNECT_CLOUD_PROJECT_ID || "",
-  chains: [mainnet, base],
-});
-
-const queryClient = new QueryClient();
+import { Providers } from "./components/Providers";
+import { AlertCard } from "./components/AlertCard";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <WagmiProvider config={config}>
-        <QueryClientProvider client={queryClient}>
-          <RainbowKitProvider modalSize="compact">
-            <ThemeProvider theme={theme}>
-              <UserProvider>
-                <AppRoutes />
-              </UserProvider>
-            </ThemeProvider>
-          </RainbowKitProvider>
-        </QueryClientProvider>
-      </WagmiProvider>
+      <Providers>
+        <AppRoutes />
+        <AlertCard />
+      </Providers>
     </BrowserRouter>
   </StrictMode>
 );
